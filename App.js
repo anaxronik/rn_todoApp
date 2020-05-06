7925d53cb386876d2290ed6861d53652601fd7c3
Sergey Komarov <anaxronik@gmail.com>
2020-05-06 15:10:04 +0500

    create alert for delete todos

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+  Alert,
+} from "react-native";
 import Navbar from "./src/components/Navbar";
 import MainScreen from "./src/screens/MainScreen";
 import TodoScreen from "./src/screens/TodoScreen";
@@ -24,7 +31,24 @@ export default function App() {
   };
 
   const deleteTodo = (id) => {
-    setTodos((prevState) => prevState.filter((todo) => todo.id != id));
+    const todo = todos.find((t) => t.id === id);
+    Alert.alert(
+      "Удаление элемента",
+      `Вы уверены, что хотите удалить ${todo.title}`,
+      [
+        {
+          text: "Отмена",
+          style: "cancel",
+        },
+        {
+          text: "УДАЛИТЬ",
+          onPress: () => {
+            setTodos((state) => state.filter((todo) => todo.id != id));
+          },
+        },
+      ],
+      { cancelable: true }
+    );
   };
 
   let pageContent = (
