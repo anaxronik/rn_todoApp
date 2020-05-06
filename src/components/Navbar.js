09ddb484b634cf75c75fdc2cfd8dbc1314dc6819
Sergey Komarov <anaxronik@gmail.com>
2020-05-06 18:27:13 +0500

    add change style for ios and android

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Platform } from "react-native";
 import { THEME } from "../theme";
 import { AppTextBold } from "./ui/AppTextBold";
 
 const Navbar = ({ title = "navbar" }) => {
   return (
-    <View style={styles.navbar}>
+    <View
+      style={{
+        ...styles.navbar,
+        ...Platform.select({
+          ios: styles.navbarIos,
+          android: styles.navbarAndroid,
+        }),
+      }}
+    >
       <AppTextBold style={styles.text}>{title}</AppTextBold>
     </View>
   );
@@ -16,13 +24,19 @@ export default Navbar;
 const styles = StyleSheet.create({
   navbar: {
     height: 70,
-    backgroundColor: THEME.MAIN_COLOR,
     alignItems: "center",
     justifyContent: "flex-end",
     paddingBottom: 10,
   },
+  navbarAndroid: {
+    backgroundColor: THEME.MAIN_COLOR,
+  },
+  navbarIos: {
+    borderBottomColor: THEME.MAIN_COLOR,
+    borderBottomWidth: 1,
+  },
   text: {
     fontSize: 28,
-    color: "white",
+    color: Platform.os === "ios" ? THEME.MAIN_COLOR : "white",
   },
 });
