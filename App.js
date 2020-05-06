01fa35f251c3747b9bf2666c5b75dc97a613ec8d
Sergey Komarov <anaxronik@gmail.com>
2020-05-06 16:57:14 +0500

    install fonts and create load function for fonts

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  FlatList,
-  Alert,
-} from "react-native";
+import * as Font from "expo-font";
+import { StyleSheet, View, Alert } from "react-native";
 import Navbar from "./src/components/Navbar";
 import MainScreen from "./src/screens/MainScreen";
 import TodoScreen from "./src/screens/TodoScreen";
 import { THEME } from "./src/theme";
+
+async function loadAplication() {
+  await Font.loadAsync({
+    "roboto-regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "roboto-bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+}
 
 export default function App() {
   const testTodos = [
